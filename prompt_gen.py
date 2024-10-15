f5b15e8912415a3c7276427a66ba08a44530a176
Jiang Yizhen <SIUBIU@users.noreply.huggingface.co>
2024-10-15 09:05:30 +0000

    Update prompt_gen.py

--- prompt_gen.py
+++ prompt_gen.py
@@ -29,7 +29,7 @@ def prompt_gen(advise, gender):
              "content": "You are a helpful assistant.",},
             {"role": "user",
              "content": advise + "根据建议，从以下的触发词、种类、上衣、裙子、领子、袖子、袖口、腰饰、裙子详述中每个挑选一个词，分点描述，触发词固定选择为" + trigger +
-                                 ", 把英文也附在后面的括号里，然后在下面先加一条prompt，以‘prompt’为开头后换行,然后输出所有英文描述，用逗"
+                                 ", 把英文也附在后面的括号里，然后在下面先加一条Prompt，以‘Prompt’为开头后换行,然后输出所有英文描述，用逗"
                                  "号间隔，再加上‘, white background’, 再然后换行后以'End'结尾；接着再加一条服饰风格介绍，以"
                                  "‘服饰风格介绍’为开头后换行，再以介绍的口吻总结输出所有的中文描述。" + prompt,
              }
